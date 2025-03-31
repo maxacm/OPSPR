8fb5ccc3e95dbef896fe98d8a61b28d75c9488d1
Max Calzada Munoz <maxcalzadamunoz98@gmail.com>
2025-03-31 15:57:38 -0400

    added first lines

--- intro_doc_scan/intro_doc_scan.py
+++ intro_doc_scan/intro_doc_scan.py
@@ -1,1 +1,23 @@
 # Python Code: OPSPR
+
+import PyPDF2
+
+# This code extracts text from a PDF
+# and adds the extracted text to a list
+def extract_text_from_pdf(pdf_file: str) -> [str]:
+    with open(pdf_file, 'rb') as pdf:
+        reader = PyPDF2.PdfReader(pdf, strict=False)
+        pdf_text = []
+        
+        for page in reader.pages:
+            content = page.extract_text()
+            pdf_text.append(content)
+        
+        return pdf_text
+
+# Run command
+
+if __name__ == "__main__": 
+  extract_text = extract_text_from_pdf('req30848.pdf') 
+  for text in extract_text:
+    print(text)
